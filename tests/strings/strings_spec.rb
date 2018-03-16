3a2691ae2ab673e04433d69ef60a1296fdcdabe7
Jason Salerno <jasons@jellyvision.com>
2018-03-16 11:35:26 -0500

    refactored basic string tests

--- tests/strings/strings_spec.rb
+++ tests/strings/strings_spec.rb
@@ -1,26 +1,23 @@
 require_relative 'strings'
 
 RSpec.describe BasicString do
+  before do
+    @test_word = "Ruby"
+    sentence = "There is a gemstone called ruby in existance"
+
+    @text = BasicString.new(sentence)
+  end
+
   context 'case-sensitive' do
     it 'should output interpolated text' do
-      test_word = "Ruby"
-      sentence = "There is a gemstone called ruby in existance"
-
-      text = BasicString.new(sentence)
-      result = text.contains_word? test_word
-
+      result = @text.contains_word? @test_word
       expect(result).to be_falsey
     end
   end
 
   context 'case-insensitive' do
     it 'should output interpolated text' do
-      test_word = "Ruby"
-      sentence = "There is a gemstone called ruby in existance"
-
-      text = BasicString.new(sentence)
-      result = text.contains_word_ignorecase? test_word
-
+      result = @text.contains_word_ignorecase? @test_word
       expect(result).to be_truthy
     end
   end
