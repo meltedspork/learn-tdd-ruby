f247e7f88fe46b81863f5145ab87ed09ee9e5379
Jason Salerno <jasons@jellyvision.com>
2018-03-26 14:09:57 -0500

    finish deck_runner.rb

--- tests/blackjack/deck_runner.rb
+++ tests/blackjack/deck_runner.rb
@@ -5,3 +5,16 @@ RANKS = ['2', '3', '4', '5', '6', '7', '8', '9', '10', 'Jack', 'Queen', 'King', 
 
 deck = Deck.new(SUITS, RANKS)
 puts deck.shuffle
+puts "\n\nDealt card details:"
+card = deck.deal_card
+puts card.rank
+puts card.suit
+
+deck_of_cards = []
+deck_of_cards.push(Card.new('Spades', '8'))
+deck_of_cards.push(Card.new('Hearts', '8'))
+deck_of_cards.push(Card.new('Diamonds', '8'))
+
+deck.replace_with deck_of_cards
+puts "\n\nNew deck of cards:"
+puts deck.shuffle
